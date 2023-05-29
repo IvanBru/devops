fc061f9bda64c4ee42b68df63143b1fa08d6f0a4
IvanBru <bryuxanyovv@gmail.com>
2023-05-29 09:01:43 +0300

    Edit practise1

--- practise1/practise1.sh
+++ practise1/practise1.sh
@@ -3,5 +3,8 @@
 # create directory for backup
 mkdir /home/ibruh/devops/practise1/backup
 
-# copy files from folder
-cp /home/ibruh/devops/practise1/ /home/ibruh/devops/practise1/backup
+# copy files from folder .sh only
+cp /home/ibruh/devops/practise1/*.sh /home/ibruh/devops/practise1/backup
+
+cp /home/ibruh/.docker/*.json /home/ibruh/devops/practise1/backup/docker
+
