2ca3fabb3e3da4b30bbb0d0c0f5f4a6b82a2b0a6
IvanBru <bryuxanyovv@gmail.com>
2023-05-29 08:58:53 +0300

    practise1

---
+++ practise1/practise1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# create directory for backup
+mkdir /home/ibruh/devops/practise1/backup
+
+# copy files from folder
+cp /home/ibruh/devops/practise1/ /home/ibruh/devops/practise1/backup
